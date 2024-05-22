f2852af877067da706341d7b63d69670ff94ac1f
heyangHEY <heyang9753124680@163.com>
2024-05-22 20:41:38 +0800

    None

--- include/ovrd_interface.hpp
+++ include/ovrd_interface.hpp
@@ -6,7 +6,7 @@
 #ifdef _WIN32
     #define DLL_EXPORT extern "C" __declspec(dllexport)
 #elif defined(__linux__)
-    #define DLL_EXPORT __attribute__((visibility("default")))
+    #define DLL_EXPORT extern "C" __attribute__((visibility("default")))
 #endif
 
 DLL_EXPORT void *HmdDriverFactory(const char *pInterfaceName, int *pReturnCode);
